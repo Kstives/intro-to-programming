25fd04c9b085ecc3877afb6f99f3e8f2c62e1eec
Karey Stivers <kareylstivers@gmail.com>
2020-03-26 01:10:10 +0000

    Added additional code examples from intro to programming book: functions gh-5

--- introduction-to-programming-book/functions/functions-notes.js
+++ introduction-to-programming-book/functions/functions-notes.js
@@ -66,3 +66,55 @@ function subtract(a, b) {
 
 console.log(add(20, 45));
 console.log(subtract(80, 10)); // examples of function composition, JS using a function call as an argument for another function
+
+function times(num1, num2) {
+  return num1 * num2;
+}
+
+console.log(times(add(20, 45), subtract(80, 10)));
+
+// above function composition same as:
+
+let sum = add(20, 45);
+let difference = subtract(80, 10);
+let result = times(sum, difference);
+console.log(result);
+
+add(subtract(80, 10), // 70
+    times(subtract(20, 6), // 14
+    add(30, 5))); // 35
+// 70 + (35 * 14) = 560
+
+/* The above function composition passes the arguments of the functions subtract() and times() to add, the function times() having the arguments of the functions subtract() and add(), this block returns 560
+*/
+
+// Three Ways to Define a Function
+
+//function declaration
+function functionName(zeroOrMoreArguments) {
+  // function body
+}
+
+//hoisted function
+greetPeople();
+
+function greetPeople() {
+  console.log('Good Morning');
+}
+
+//function expression
+let greetAnimals = function() {
+  console.log('ooo-ooo, ahh-ahh');
+};
+
+greetAnimals();
+
+// You cannot evoke a function expression before declaring it
+
+//arrow function
+let greetAliens = () => console.log('Greetings, martians');
+greetAliens();
+
+// Useful in the context of OOP, it features implicit returns and only one expression in the function's body
+
+let division = (a, b) => a / b;
