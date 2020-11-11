fb34bbb73ffdedca426f3a7ee56ef232bf5952e6
Kstives <kareylstivers@gmail.com>
2020-11-10 16:50:05 -0800

    Correcting previous commit ff575b7846c7492a46678e49e2f14ae87202df0d which contained  improper file names and exercise count.

--- input-output/exercises/greeter.js
+++ input-output/exercises/greeter.js
@@ -1,3 +1,10 @@
+// exercise 1
+// let rlSync = require('readline-sync');
+// let name = rlSync.question('What is your name?\n');
+// console.log(`Hello, ${name}!`);
+
+// exercise 2
 let rlSync = require('readline-sync');
-let name = rlSync.question('What is your name?\n');
-console.log(`Hello, ${name}!`);
+let firstName = rlSync.question('What is your first name? ');
+let lastName = rlSync.question('What is your last name? ');
+console.log(`Hello, ${firstName} ${lastName}!`);
